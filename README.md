2617e7b665d827588c5e75c98c5cdcbded9f5805
thovisjo <35284895+thovisjo@users.noreply.github.com>
2018-01-26 11:48:48 -0500

    Finished
    This is a now finished assignment.

--- README.md
+++ README.md
@@ -1,38 +1,40 @@
 # 02-Guess-the-number
 
-Guess the number! This is the second programming assignment for ILS-Z399. The requirements for this assignment are as follows:
+In this guess the number game, I wanted to play around with certain aspects of the game.
+In most guess the  number games, the game is over once the number is guessed.
+This one plays more like an older game focused on high scores; the game ends
+after the player fails to guess the number.
 
-* Write a Python program for playing a simple "guess the number" game
-* The program should randomly choose a number and then invite the user to guess the chosen number. 
-* A game is only fun if it holds out a possibility for mastery, so the user will need to be given multiple chances to guess.
-* Try to give the player useful feedback; you can decide how many chances the player will get.
+Though the game systems are functional, I believe it could use work.
 
-To get a perfect score, you will need to think creatively! Think about ways you can make this project more interesting or fun.
+The basics of the game:
+* There are 10 hours to attempt to open the door, which is locked with a code.
+* The code is cracked via the guess the number game, from 1-100.
+* Since it takes, at maximum, seven tries to correctly guess a number from 1-100, there would be no way to lose, unless
+* There's a system in place to prevent winning forever. The player will slowly lose food,
+* and, once the player starts being hungry, at zero food, they will oversleep.
+* This will lower the amount of hours the player has to guess the code.
+* To try to survive longer, the player can search for food, spending valuable hours.
+* The chances of gathering food  are random, and include 5 random encounters which have choices.
+* Even with this system in place, the player will still slowly lose food.
+* The player can also quit at any time other than in a random encounter by typing quit.
 
-To complete this assignment you will need to understand:
+Things that could be further improved:
+* On the random encounters and other response driven questions, the input is functional but finnicky.
+* More random encounters.
+* More description to increase interest in the game.
+* Fix the provisions counter so that it is more accurate (Kinda fixed)
+* Describe the systems within the game more clearly.
+* Allow significantly more input options.
+* Better balance the scavenge system; its a little too rewarding.
+* Clean up the code just a little.
 
-* Python [integers and strings](https://docs.python.org/3.7/library/stdtypes.html)
-* How to [convert a string into an integer](https://stackoverflow.com/questions/379906/parse-string-to-float-or-int)
-* How to generate a [random integer](https://docs.python.org/3.6/library/random.html)
-* How to use basic control structures like [if and else (and elif)](http://anh.cs.luc.edu/python/hands-on/3.1/handsonHtml/ifstatements.html)
-* How to write a [for loop over a range](https://www.learnpython.org/en/Loops)
-* How to [compare](https://www.tutorialspoint.com/python/comparison_operators_example.htm) two numbers in Python
-* How to [accept user input](http://anh.cs.luc.edu/python/hands-on/3.1/handsonHtml/io.html)
+Code wise, there are many features within the game:
+* While and nested while loops. I chose a while loop so that the game could be exited manually and to better determine when the game ends.
+* There are functions within the code for each of the random encounters. This allows them to be possibly used later.
+* Everything that has a chance of failing is randomly determined, similar to a dice roll in D&D
+* if, elif, and else all find there way into the code at various points.
+* While loops are also used in the functions of the random encounters to prevent typos affected choices.
+* Practically every string printed has escape characters.
 
-I have given you a few lines of code to get started, but you will need to alter guess.py substantially to complete the assignment. Good luck!
-
----
-
-The grading criteria will be as follows:
-
-* [1 point] Assignment turned in on time
-* [1] Repository contains an appropriate software license
-* [2] Repository contains a descriptive README.md
-* [1] Requires Python 3
-* [1] No syntax errors
-* [8] Accomplishes the objective of the assignment
-* [2] No other runtime errors
-* [2] Validates user input (if necessary)
-* [2] Adds interesting features (beyond the scope of the assignment)
-
-20 points total
+That's about it!
--- guess.py
+++ guess.py
@@ -4,26 +4,208 @@ import sys, random
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-#variables
-guesses = 10
-guess_range = 20
+def random_encounter_generator(num60to89, provision):
+        """Randomly chooses between 5 pregenerated scenarios.
+
+        num -> function"""
+
+        if(num60to89 < 66):
+                return random_encounter_horde()
+
+        elif(num60to89 < 72):
+                return random_encounter_bandits(provision)
+
+        elif(num60to89 < 78):
+                return random_encounter_dogs()
+
+        elif(num60to89 < 84):
+                return random_encounter_helpinghand(provision)
+
+        elif(num60to89 < 90):
+                return random_encounter_deer(provision)
+
+def random_encounter_horde():
+        while True:
+                dec = str.upper(input("A sea of the living dead stands before you. You must try to sneak past it or return to the door. Please enter \"Return\" or \"Sneak\".\n"))
+                if(dec == "SNEAK"):
+                        if random.randint(0,100) > 75:
+                                print("You have found a can of pineapple juice in a shack. Not much, but it will sustain you for a day.")
+                                return 1
+                        else:
+                                print("The effort was fruitless; there was simply no way around the horde. You return home empty-handed.")
+                                return 0
+                elif(dec == "RETURN"):
+                        print("You turn back in the face of the horde, with nothing to show for your wasted time.")
+                        return 0
+                else:
+                        print("Please enter \"Return\" or \"Sneak\".")
+
+def random_encounter_bandits(amtfood):
+        while True:
+                dec = str.upper(input("""An armed man, covered in dirt, sits on a blue chair in front of you.""" +
+                                      """There is a can of tomato soup under his chair. Will you fight him for the can or leave?\nPlease enter \"Fight\" or \"Leave\".\n"""))
+                if(dec == "FIGHT"):
+                        if random.randint(0,100) > 50:
+                                print("You successfully surprise the gruff man, choking him and stealing his meal. He would do the same, after all.")
+                                return 1
+                        else:
+                                print("The strong man overpowers you and begins to fire at you. You barely escape and, in your haste, drop a can of food.")
+                                if amtfood > 0:
+                                        return -1
+                                else:
+                                        return 0
+                elif(dec == "LEAVE"):
+                        print("You do not trust that you could overpower this man. You return home empty-handed.")
+                        return 0
+
+                else:
+                        print("Please enter \"Fight\" or \"Leave\".")
+
+def random_encounter_dogs():
+        while True:
+                dec = str.upper(input("""A group of wild dogs - perhaps once domesticated - are whining outside a door inside an overgrown house.""" +
+                                      """\nWill you attempt to open the door? Please enter \"Yes\" or \"No\".\n"""))
+                if(dec == "YES"):
+                        if random.randint(0, 100) > 50:
+                                print("The wild dogs allow you to open the door for them. They begin feasting on the corpse within, distracted while you grab a can of dog food for yourself.")
+                                return 1
+                        else:
+                                print("The wild dogs bark and chase you away from the door. You remain uninjured, but are forced to return to the bunker empty-handed.")
+                                return 0
+                elif(dec == "NO"):
+                        print("Deciding not risk upsetting the canines, you return home without anything to show for your trip.")
+                        return 0
+
+                else:
+                              print("Please enter \"Yes\" or \"No\".")
+
+def random_encounter_helpinghand(amtfood):
+        while True:
+                dec = str.upper(input("A man stands alone in a clearing. He is well-armed. Do you approach? Please enter \"Yes\" or \"No\".\n"))
+                if dec == "YES":
+                        if random.randint(0,100) > 30:
+                                print("The man spots you, and waves at you. He has a smile on his face. After a short conversation, he agrees to give you a can of tuna and sends you on your way.")
+                                return 1
+                        else:
+                                print("The man spots you and waves you over. When you get close, however, he holds you at gunpoint and demands a can of food. You are forced to give him one.")
+                                if amtfood > 0:
+                                        return -1
+                                else:
+                                        return 0
+                elif dec == "NO":
+                        print("No other oppurtunities present themselves. You find no food and are forced to return to the bunker with nothing.")
+                        return 0
+
+                else:
+                        print("Please enter \"Yes\" or \"No\".")
+
+def random_encounter_deer(amtfood):
+        while True:
+                dec = str.upper(input("A great stag has impaled itself upon a piece of rebar and has died. Will you harvest what you can? Please enter \"Yes\" or \"No\".\n"))
+                if dec == "YES":
+                        if random.randint(0,100) > 10:
+                                print("The stag's meat provides two days of much-appreciated fresh meat to your diet after you collect what you can.")
+                                return 2
+                        else:
+                                print("When you eat the meat, you come to realize it has made you even more starving than before. You are forced to eat a can of food to survive.")
+                                if amtfood > 0:
+                                        return -1
+                                else:
+                                        return 0
+
+                elif dec == "NO":
+                        print("You cannot risk getting sick from meat, no matter how fresh. You unfortunately return home empty handed.")
+                        return 0
+
+                else:
+                        print("Please enter \"Yes\" or \"No\".")
 
 
-#generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
-number = random.randint(1,guess_range)
 
-#ask the user for a response and store it in the variable [guess]
-guess = input()
+#declaring variables
+
+#Guesses which can be depleted by other activities, such as searching for food or being exhausted by lack of food.
+hours = 10
+#A player can guess between 1 and 100.
+guess_range = 100
+# The player has 3 nights of food to start with. When it reaches zero, the player will begin to oversleep.
+provisions = 3
+#Nights are the score.
+nights = 0
+# Keeps track of whether the player got inside.
+inside = 0
+# Luck determines whether a scavenging session is successful.
+luck = 0
+# To exit the game early
+end = False
+
+#generate a random integer between 1 and 100 (inclusive) and store it in the variable [number]
+
+# Main set of code
+
+while(hours > 0 and not end):
+        nightsSTR = str(nights)
+        provisionsSTR = str(provisions)
+        print("You have survived " + nightsSTR + " nights. \nYou have " + provisionsSTR + " nights of food.\nEnter \"Quit\" to quit.")
+        inside = 0
+        combo = random.randint(1,guess_range)
+        while(hours > 0 and not end):
+                scavOrGuess = input("Would you like to scavenge for food or guess the combo? Each takes one hour.\n")
+
+                if(str.upper(scavOrGuess) == ("SCAVENGE") or str.upper(scavOrGuess) == ("SCAVENGE FOR FOOD") or str.upper(scavOrGuess) == "FOOD"):
+                        hours -= 1
+                        luck = random.randint(1,100)
+                        if(luck < 60):
+                                print("You found no food in the overgrown buildings surrounding the bunker.")
+                        elif(luck >= 60 and luck <90):
+                                provisions = provisions + random_encounter_generator(luck, provisions)
+                        elif(luck >= 90):
+                                print("You found a delicious, beautiful can of baked beans in the burnt-out rubble of a house. Provisions +1. ")
+                                provisions += 1
+
+                elif(str.upper(scavOrGuess) == ("GUESS") or str.upper(scavOrGuess) == ("GUESS THE COMBO") or str.upper(scavOrGuess) == ("COMBO")):
+                        while(hours > 0 and not end):
+                                #ask the user for a response and store it in the variable [guess]
+                                guess = input("Input a combination between 1 and 100.\n")
 
 
-#a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
-try:
-	#convert the guess to an integer
-	guess = int(guess)
+                                #a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
+                                try:
+                                        #convert the guess to an integer
+                                        guess = int(guess)
 
-	#check if the guess is less than the random number
-	if guess < number:
-		print('Too low!')
+                                #check if the guess is less than the random number
+                                        if guess < combo:
+                                                print('Too low!')
+                                                hours -= 1
 
-except ValueError:
-	print('Please enter a whole number.')
+                                        elif guess > combo:
+                                                print("Too high!")
+                                                hours -= 1
+
+                                        elif guess == combo:
+                                                print("You got in!")
+                                                nights += 1
+                                                hours = 10
+                                                inside = 1
+                                                break
+
+                                        hoursSTR = str(hours)
+                                        print("You have " + hoursSTR + " hours left.")
+                                except ValueError:
+                                        if str.upper(guess) == "QUIT":
+                                                end = True
+                                        else:
+                                                print('Please enter a whole number.')
+                        if(inside == 1):
+                                break
+                elif(str.upper(scavOrGuess) == "QUIT"):
+                     end = True
+        if(provisions == 0):
+                hours -= random.randint(3, 6)
+                hoursSTR = str(hours)
+                print("Due  to lack of food, you overslept. You only have " + hoursSTR + " hours today.")
+        elif(provisions > 0):
+                provisions -= 1
+
+print("You have died. You survived " + nightsSTR + " nights.")
